72699beb764378cd7804ad4d6ee8bff8a3ede288
KozielGPC <gpcgabriel0@gmail.com>
2022-07-31 20:43:21 -0300

    update: set env socket url

--- src/components/Socket/index.ts
+++ src/components/Socket/index.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io(process.env.REACT_APP_API_URL ?? 'http://localhost:3000');
 
 export default socket;
