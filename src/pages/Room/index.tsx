f7937fcae398b176c958d6b998699a88b3a93653
KozielGPC <gpcgabriel0@gmail.com>
2023-11-02 21:56:06 -0300

    #7 - update emotes and users list to component

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import Draw from '../../components/Draw';
 import Answer from '../../components/Answer';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,36 +6,9 @@ import { v4 as uuidv4 } from 'uuid';
 import socket from '../../components/Socket/index';
 import './styles.css';
 
-import cavaloIcon from '../../assets/emotes/cavalo2.png';
-import ruimIcon from '../../assets/emotes/ruim2.png';
-import surpriseIcon from '../../assets/emotes/surprise.png';
-import tanIcon from '../../assets/emotes/tan2.png';
-import eoqIcon from '../../assets/emotes/eoq.png';
-import faustaoIcon from '../../assets/emotes/faustao.png';
-import chavesIcon from '../../assets/emotes/chaves.png';
-import macacoIcon from '../../assets/emotes/macaco.png';
-import facepalmIcon from '../../assets/emotes/facepalm2.png';
-import peidoIcon from '../../assets/emotes/peido.png';
-import rojaoIcon from '../../assets/emotes/rojao.png';
-
-// const eoq = require('../../assets/sounds/eoq.mp3');
-// const burro = require ('../../assets/sounds/burro.mp3');
-// const cavalo = require ('../../assets/sounds/cavalo.mp3');
-// const errou = require ('../../assets/sounds/errou.mp3');
-// const facepalm = require ('../../assets/sounds/facepalm.mp3');
-// const macaco = require ('../../assets/sounds/olha-o-macaco.mp3');
-// const peido = require ('../../assets/sounds/peido.mp3');
-// const rojao = require ('../../assets/sounds/rojao.mp3');
-// const shock = require ('../../assets/sounds/shock.mp3');
-// const tan = require ('../../assets/sounds/tan.mp3');
-// const surprise = require ('../../assets/sounds/surprise.mp3');
-// const ruim = require ('../../assets/sounds/ruim.mp3');
-
 import ShowDraw from '../../components/ShowDraw';
-import { useUser } from '../../hooks/useUser';
 import { Room, RoomPlayers } from '../../interfaces/iRoom';
 import { User } from '../../interfaces/iUser';
-import { useRoom } from '../../hooks/useRoom';
 import { useMatch } from '../../hooks/useMatch';
 import { EndMatch, Match, MatchRounds, Round } from '../../interfaces/iMatch';
 import { useRound } from '../../hooks/useRound';
@@ -44,21 +16,8 @@ import { Content, EnumRoundType, ReceivingRound } from '../../interfaces/iRound'
 import api from '../../providers';
 import Chat from '../../components/Chat';
 import RoomInfo from '../../components/RoomInfo';
-
-// const soundsList = [
-//     { sound: eoq, idSound: 1 },
-//     { sound: burro, idSound: 2 },
-//     { sound: cavalo, idSound: 3 },
-//     { sound: errou, idSound: 4 },
-//     { sound: facepalm, idSound: 5 },
-//     { sound: macaco, idSound: 6 },
-//     { sound: peido, idSound: 7 },
-//     { sound: rojao, idSound: 8 },
-//     { sound: shock, idSound: 9 },
-//     { sound: tan, idSound: 10 },
-//     { sound: surprise, idSound: 11 },
-//     { sound: ruim, idSound: 12 },
-// ]
+import UsersList from '../../components/UsersList';
+import EmotesList from '../../components/EmotesList';
 
 export default function RoomPage() {
     const { createMatch } = useMatch();
@@ -243,10 +202,6 @@ export default function RoomPage() {
         });
     }, []);
 
-    function handleEmote(idEmote: any) {
-        // socket.emit('click-emote', idEmote);
-    }
-
     async function handleCreateGame() {
         try {
             if (phrase.length === 0) {
@@ -391,66 +346,8 @@ export default function RoomPage() {
             </div>
 
             <div className="side">
-                <div className="users">
-                    <h1>Users</h1>
-                    {/* <h1>Showadm {showAdm ? 'sim' : 'nao'}</h1> */}
-                    <div className="players">
-                        <ul>
-                            {players?.users.map((player) => (
-                                <li key={player.user_id}>
-                                    {admNick === player.user.username ? (
-                                        <span className="admin">{player.user.username} </span>
-                                    ) : (
-                                        <span className="player">{player.user.username} </span>
-                                    )}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-                <div className="emotes">
-                    <div className="emotes-title">
-                        <h1>Emotes</h1>
-                    </div>
-                    <div className="emotes-button">
-                        <button className="emote-button" onClick={() => handleEmote(1)}>
-                            <img src={eoqIcon} alt="eoq" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(2)}>
-                            <img src={chavesIcon} alt="chaves-aiqburro" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(3)}>
-                            <img src={cavaloIcon} alt="cavalo" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(4)}>
-                            <img src={faustaoIcon} alt="faustao-errou" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(5)}>
-                            <img src={facepalmIcon} alt="facepalm" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(6)}>
-                            <img src={macacoIcon} alt="macaco" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(7)}>
-                            <img src={peidoIcon} alt="peido" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(8)}>
-                            <img src={rojaoIcon} alt="rojao" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(9)}>
-                            <img src={tanIcon} alt="tan-tan-taaan" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(10)}>
-                            <img src={tanIcon} alt="tan-tan-taaan" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(11)}>
-                            <img src={surpriseIcon} alt="surprise-motherf*cker" />
-                        </button>
-                        <button className="emote-button" onClick={() => handleEmote(12)}>
-                            <img src={ruimIcon} alt="ruim-piorou" />
-                        </button>
-                    </div>
-                </div>
+                <UsersList adm_nick={admNick ?? ''}  players={players ?? { users: [], room_adm: user }}/>
+                <EmotesList />
             </div>
         </div>
     );
